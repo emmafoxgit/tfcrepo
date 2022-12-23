34c61a8c7761fc0ba7556152e32b796031e6a762
tony@foxmax.onmicrosoft.com <fowlert>
2022-12-23 17:06:12 +0000

    geo

--- main.tf
+++ main.tf
@@ -35,5 +35,20 @@ resource "azurerm_cosmosdb_account" "name" {
     offer_type = "Standard"
     kind = "MongoDB"
     enable_automatic_failover = true
+
+    consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
   
+  geo_location {
+    location          = "eastus"
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = "westus"
+    failover_priority = 0
+  }
 }
