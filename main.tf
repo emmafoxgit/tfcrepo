e86a184f320b2dac7f88598fa6ad1e591c25322e
tony@foxmax.onmicrosoft.com <fowlert>
2022-12-25 15:01:21 +0000

    q

--- main.tf
+++ main.tf
@@ -19,11 +19,13 @@ resource "azurerm_resource_group" "name" {
   location = var.location
 }
 
-resource "azurerm_storage_account" "name" {
-    name = var.storage_account
-    resource_group_name = var.resource_group_name
-    location = var.location
-    account_tier = "Standard"
-    account_replication_type = "LRS"
+#resource "azurerm_storage_account" "name" {
+ #   name = var.storage_account
+  #  resource_group_name = var.resource_group_name
+   #
+   # location = var.location
+    #account_tier = "Standard"
+    
+    #account_replication_type = "LRS"
   
-}
+#}
