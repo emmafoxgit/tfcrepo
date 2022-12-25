91c35af6e2a03ab359ce4fb12c75dbda0994b85b
tony@foxmax.onmicrosoft.com <fowlert>
2022-12-25 10:34:18 +0000

    remove

--- main.tf
+++ main.tf
@@ -27,28 +27,3 @@ resource "azurerm_storage_account" "name" {
     account_replication_type = "LRS"
   
 }
-
-resource "azurerm_cosmosdb_account" "name" {
-    name=var.cosmos_db_account
-    location = var.location
-    resource_group_name = var.resource_group_name
-    offer_type = "Standard"
-    kind = "MongoDB"
-    enable_automatic_failover = true
-
-    consistency_policy {
-    consistency_level       = "BoundedStaleness"
-    max_interval_in_seconds = 300
-    max_staleness_prefix    = 100000
-  }
-  
-  geo_location {
-    location          = "eastus"
-    failover_priority = 1
-  }
-
-  geo_location {
-    location          = "westus"
-    failover_priority = 0
-  }
-}
