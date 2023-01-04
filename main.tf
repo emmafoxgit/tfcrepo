ad468e0405d933a6192cd3e0a1343a2b90af7d89
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2023-01-04 16:17:57 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,20 +29,5 @@ resource "azurerm_key_vault" "example" {
 
   sku_name = "standard"
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-  }
+  
 }
