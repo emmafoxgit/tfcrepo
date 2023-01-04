37e6817ba38bd6c5266a1a262c806b8e125a3cd6
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2023-01-04 16:12:16 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,21 +11,46 @@ terraform {
 provider "azurerm" {
   features {}
 }
-#
 
-
-resource "azurerm_resource_group" "name" {
-  name = var.resource_group_name
-  location = var.location
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
 }
 
-#resource "azurerm_storage_account" "name" {
- #   name = var.storage_account
-  #  resource_group_name = var.resource_group_name
-   #
-   # location = var.location
-    #account_tier = "Standard"
-    
-    #account_replication_type = "LRS"
-  
-#}
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "example" {
+  name     = "tfow-rg-today"
+  location = "West Europe"
+}
+
+resource "azurerm_key_vault" "example" {
+  name                        = "examplekeyvault"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
