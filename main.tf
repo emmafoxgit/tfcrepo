63ba01b3a8e24730222b5db7aa630a9cd3f696f9
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2023-01-04 16:16:16 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,14 +12,7 @@ provider "azurerm" {
   features {}
 }
 
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
- 
-
-data "azurerm_client_config" "current" {}
-
+   
 resource "azurerm_resource_group" "example" {
   name     = "tfow-rg-today"
   location = "West Europe"
